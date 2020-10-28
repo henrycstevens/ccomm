870c1f18a97dac9216e6629a2ddd74677f045836
henrycstevens <henrycstevens@gmail.com>
2020-10-28 15:21:22 -0400

    updated documentation

---
+++ R/community.R
@@ -0,0 +1,17 @@
+#' community data
+#' 
+#' Dataset on Ecuadorian bird communities to be used as examples in the functions
+#' 'givemeNMDS' and 'givemeDIVERSITY'. Organized as site (rows) by species 
+#' (columns) matrix with count data in each cell. 
+#' 
+#' @docType data
+#' 
+#' @usage data(community)
+#' 
+#' @examples 
+#' 
+#' comm <- data(community)
+#' givemeNMDS(comm)
+#' givemeDIVERSITY(comm)
+
+"community"
---
+++ R/traits.R
@@ -0,0 +1,17 @@
+#' functional traits data
+#' 
+#' Dataset on Ecuadorian bird communities to be used as example in the function 
+#' 'givemeTREE'. Organized as species (rows) by traits (columns) matrix with 
+#' binary 0/1 data in each cell (0 = species does not have trait, 1 = species 
+#' has trait). 
+#' 
+#' @docType data
+#' 
+#' @usage data(traits)
+#' 
+#' @examples
+#' 
+#' traits <- data(traits)
+#' givemeTREE(traits)
+
+"traits"
